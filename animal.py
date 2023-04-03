85a6c68c9e4295abdb32d3572fd62ffbb90004f2
sandekap <sandekap@oregonstate.edu>
2023-04-03 10:47:34 +0100

    first assignment

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Kapri Sanders
+# Github username: sandekap
+# Date: 04/02/2023
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("My favorite animal is the " + fave_animal + ".")
