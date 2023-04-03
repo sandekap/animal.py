2013b3f49386f4b2c0431d3a320ccc89bfe4c1d1
sandekap <sandekap@oregonstate.edu>
2023-04-03 10:52:04 +0100

    first assignment

--- animal.py
+++ animal.py
@@ -4,4 +4,4 @@
 # Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
 
 fave_animal = input("Please enter your favorite animal: ")
-print("My favorite animal is the " + fave_animal + ".")
+print("Your favorite animal is the " + fave_animal + ".")
